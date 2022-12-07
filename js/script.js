ff4a88a61eb76174665a0ab7c01964611d75be60
Rodrigo <rodrigovinicius.bsb@gmail.com>
2022-12-07 17:44:55 -0300

    Commit final project
    -Create file blogApi.json
    
    -Create function fetch
    
    - remove undeline hover in section blog

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ let navbar = document.querySelector('.navbar');
 let link = document.querySelectorAll('.nav-link');
 let sections = document.querySelectorAll('section');
 
+let url = '../project-amazonia/blogAPI.json';
+
 function initNavBar() {
   menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
@@ -72,93 +74,51 @@ function initCarroselEffect() {
   });
 }
 
-let json = [
-  {
-    calendar: "<i class='fas fa-calendar'></i>17/11/2022",
-    title: 'Reciclagem',
-    paragraph:
-      'A reciclagem ajuda a conservar recursos naturais como madeira, água e minerais, o que reduz a extração de novas matérias-primas. Quanto mais...',
-  },
-  {
-    calendar: "<i class='fas fa-calendar'></i> 2/11/2022",
-    title: 'Luta pela sobrevivência',
-    paragraph:
-      'O povo indígena yanomami que há séculos ocupa a floresta amazônica enfrenta diveros desafios atuais. A luta contra exploradores ...',
-  },
-  {
-    calendar: "<i class='fas fa-calendar'></i>15/10/2022 ",
-    title: 'Vacinas Covid-19',
-    paragraph:
-      'Na última semana de maio, 10 comunidades receberam doses da vacina contra a Covid-19. A vacina foi destinada desde crianças até adultos...',
-  },
-  {
-    calendar: "<i class='fas fa-calendar'></i> 1/10/2022 ",
-    title: 'Pecuária sustentável',
-    paragraph:
-      'A pecuária sustentável são um conjunto de técnicas com consciência ambiental e responsabilidade social...',
-  },
-  {
-    calendar: "<i class='fas fa-calendar'></i> 22/09/2022 ",
-    title: 'Tecnologia para todos',
-    paragraph:
-      'Essa iniciativa foi criada para apoiar povos e e ensina-los sobre as tecnologias e como elas são úteis...',
-  },
-  {
-    calendar: "<i class='fas fa-calendar'></i> 07/09/2022 ",
-    title: 'Desmatamento',
-    paragraph:
-      'Certas atividades humanas, como o desmatamento, estão levando espécies à extinção em um ritmo alarmante. Estudos mostram que...',
-  },
-];
+async function getAllPosts() {
+  let response = await fetch(url);
 
-let card = document.querySelector('.card');
+  let data = await response.json();
 
-for (let contador = 1; contador <= json.length; contador++) {
-  let div = document.createElement('div');
-  div.classList.add('box', 'swiper-slide', 'post-js');
-  card.appendChild(div);
+  let count = 1;
+
+  data.map((contentPost) => {
+    let divCard = document.createElement('div');
+    divCard.classList.add('box', 'swiper-slide', 'post-js');
+    document.querySelector('.card').appendChild(divCard);
+
+    let img = document.createElement('img');
+    img.className = 'img-post';
+    img.setAttribute('src', `images/img${count}.jpg`);
+    divCard.appendChild(img);
+
+    let divTextPost = document.createElement('div');
+    divTextPost.classList.add('content', 'content-js');
+    divCard.appendChild(divTextPost);
+
+    let span = document.createElement('span');
+    let h3 = document.createElement('h3');
+    let p = document.createElement('p');
+    let a = document.createElement('a');
+
+    span.className = 'calendar';
+    h3.className = 'text-title';
+    p.className = 'text-paragraph';
+    a.className = 'btn-post';
+
+    span.innerHTML = `<i class='fas fa-calendar'></i>${contentPost.calendar}`;
+    h3.innerText = contentPost.title;
+    p.innerText = contentPost.paragraph;
+    a.innerHTML = "<button class='btn'> Leia mais</button>";
+
+    divTextPost.appendChild(span);
+    divTextPost.appendChild(h3);
+    divTextPost.appendChild(p);
+    divTextPost.appendChild(a);
+    count++;
+  });
 }
-
-let boxPost = document.querySelectorAll('.post-js');
-console.log(boxPost);
-
-boxPost.forEach((element, index) => {
-  //imagem do post
-  let img = document.createElement('img');
-
-  img.className = 'img-post';
-  element.appendChild(img);
-  element
-    .querySelector('.img-post')
-    .setAttribute('src', `images/img${index + 1}.jpg`);
-
-  let div = document.createElement('div');
-  div.classList.add('content', 'content-js');
-
-  element.appendChild(div);
-
-  //conteudo do post
-  let span = document.createElement('span');
-  let h3 = document.createElement('h3');
-  let p = document.createElement('p');
-  let a = document.createElement('a');
-
-  span.className = 'calendar';
-  h3.className = 'text-title';
-  p.className = 'text-paragraph';
-  a.className = 'btn-post';
-
-  span.innerHTML = json[index].calendar;
-  h3.innerText = json[index].title;
-  p.innerText = json[index].paragraph;
-  a.innerHTML = "<button class='btn'> Leia mais</button>";
-
-  element.querySelector('.content-js').appendChild(span);
-  element.querySelector('.content-js').appendChild(h3);
-  element.querySelector('.content-js').appendChild(p);
-  element.querySelector('.content-js').appendChild(a);
-});
 
 initNavBar();
 initActiveNavLink();
 initCarroselEffect();
+getAllPosts();
